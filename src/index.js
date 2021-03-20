e13558af2984d1980928da97c5312b4393c7cf08
tannerfronk <tannerjace07@hotmail.com>
2021-03-20 15:48:22 -0600

    City Selection works and rerenders

--- src/index.js
+++ src/index.js
@@ -3,102 +3,106 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import WeatherInfo from './weather.json'
 
-
-
-let name = 'Salt Lake City'
-console.log(WeatherInfo[name][0].days[0].highF)
-let degree;
-
-function FahrenheitWeatherTile({value}){
-  return(
-    <div className="square">
-      <h2>Current Temperature: {WeatherInfo[name][0].currentTempF}</h2>
-      <h3>High: {WeatherInfo[name][0].days[value].highF}</h3>
-      <h3>Low: {WeatherInfo[name][0].days[value].lowF}</h3>
-
-    </div>
-  )
-}
-function CelsiusWeatherTile({value}){
-  return(
-    <div className="square">
-      <h2>Current Temperature: {WeatherInfo[name][0].currentTempC}</h2>
-      <h3>High: {WeatherInfo[name][0].days[value].highC}</h3>
-      <h3>Low: {WeatherInfo[name][0].days[value].lowC}</h3>
-
-    </div>
-  )
-}
-
-function DegreeTypeBtn({onClick}){
-  return(
-    <button id="degreeBtn" onClick={onClick}>
-      Change Degree Type
-    </button>
-  )
-}
-
-function CitySelect({onSubmit}){
-  return(
-    <form name="citySelect">
-      <input id="citySelect" placeholder="Type in your City" onSubmit={onSubmit}>
-      
-      </input>
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
-
 function Weather() {
 
   const [ isFahrenheit, setIsFahrenheit ] = useState(true)
-  const [ name, setName] = useState()
-  
-  degree = isFahrenheit ? 'F' : 'C'
+  const [ name, setName ] = useState('Salt Lake City')
   console.log(name)
 
-function renderCitySelect(){
-  return (<CitySelect 
-  onSubmit = {() => {
+  function renderCitySelect(){
+    return <CitySelect 
+      onClick = {() => {
+        setName(document.getElementById('citySelect').value)
+        ReactDOM.render(
+          <Weather />,
+          document.getElementById('root')
+        );
+      }}
+    />
+  }
 
-    setName = document.getElementById('citySelect').value
-  }}
-  />
-  )
-}
+  function FahrenheitWeatherTile({value}){
+    if(name === ""){
+      return(
+        <div className="square">
+          <p>No data for selected City</p>
+        </div>
+      )
+    } else {
+      return(
+        <div className="square">
+          <h2>Current Temperature: {WeatherInfo[name][0].currentTempF}</h2>
+          <h3>High: {WeatherInfo[name][0].days[value].highF}</h3>
+          <h3>Low: {WeatherInfo[name][0].days[value].lowF}</h3>
+        </div>
+      )
+    }
+  }
 
-function renderWeatherTile(i){
-  if(isFahrenheit === true){
-    return <FahrenheitWeatherTile
-    value = {i}
-     />
-  } else if(isFahrenheit === false){
-    return <CelsiusWeatherTile
-    value = {i}
-   />
+  function CelsiusWeatherTile({value}){
+    if(name === ""){
+      return(
+        <div className="square">
+          <p>No data for selected City</p>
+        </div>
+      )
+    } else {
+      return(
+        <div className="square">
+          <h2>Current Temperature: {WeatherInfo[name][0].currentTempC}</h2>
+          <h3>High: {WeatherInfo[name][0].days[value].highC}</h3>
+          <h3>Low: {WeatherInfo[name][0].days[value].lowC}</h3>
+        </div>
+      )
+    }
   }
   
-}
-function renderChangeDegreeTypeBtn(){
-  return <DegreeTypeBtn 
-  onClick={() => {
-    setIsFahrenheit(!isFahrenheit)
-  }}
-  />
-}
-  return (
-    <div className="container">
-      {renderCitySelect()}
-      {renderChangeDegreeTypeBtn()}
-      <div className="currentWeather">
-        {renderWeatherTile(1)}
-
+  function DegreeTypeBtn({onClick}){
+    return(
+      <button id="degreeBtn" onClick={onClick}>
+        Change Degree Type
+      </button>
+    )
+  }
+  
+  function CitySelect({onClick}){
+    return(
+      <div>
+        <input id="citySelect" placeholder={name}>
+        
+        </input>
+        <button onClick={onClick}>Get Weather</button>
       </div>
-
-
-
-    </div>
-  )
+    )
+  }
+  function renderWeatherTile(i){
+    if(isFahrenheit === true){
+      return <FahrenheitWeatherTile
+      value = {i}
+      />
+    } else if(isFahrenheit === false){
+      return <CelsiusWeatherTile
+      value = {i}
+    />
+    }
+    
+  }
+  function renderChangeDegreeTypeBtn(){
+    return <DegreeTypeBtn 
+    onClick={() => {
+      setIsFahrenheit(!isFahrenheit)
+    }}
+    />
+  }
+    return (
+      <div className="container">
+          {renderCitySelect()}
+          {renderChangeDegreeTypeBtn()}
+        <div className="currentWeather">
+          {renderWeatherTile(0)}
+        </div>
+      </div>
+    )
 
 }
 
