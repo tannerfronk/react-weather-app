6644303527b6620b9f601ced119afcaa73cb21e9
tannerfronk <tannerjace07@hotmail.com>
2021-03-20 12:26:36 -0600

    Started weather app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function WeatherTile(){
+  return(
+    <div className="square">
+      <h2>Current Temperature: </h2>
+
+    </div>
+  )
+}
+
+function Weather() {
+
+function renderWeatherTile(){
+  return <WeatherTile />
+}
+  return (
+    <div className="container">
+      <div classNam="currentWeather">
+        {renderWeatherTile()}
+
+      </div>
+
+
+
+    </div>
+  )
+
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Weather />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
