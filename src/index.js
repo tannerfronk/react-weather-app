17272bcfee4e9b9b031b0adc198c7ed183b186b3
tannerfronk <tannerjace07@hotmail.com>
2021-03-20 18:22:13 -0600

    Weather App works

--- src/index.js
+++ src/index.js
@@ -21,6 +21,151 @@ function Weather() {
     />
   }
 
+  function TodaysTile({value}){
+    if(name === ""){
+      return(
+        <div className="square">
+          <p>No data for selected City</p>
+        </div>
+      )
+    } else if(isFahrenheit === true){
+      return(
+        <div className="todaySquare">
+          <div className ="todayTop">
+            <h2>Today's Date: {WeatherInfo[name][0].days[0].date}</h2>
+            <h2>Current Temperature: {WeatherInfo[name][0].currentTempF}</h2>
+            <h3>High: {WeatherInfo[name][0].days[value].highF}</h3>
+            <h3>Low: {WeatherInfo[name][0].days[value].lowF}</h3>
+            <h3>Chance of precipitation: {WeatherInfo[name][0].days[value].chance}</h3>
+            <h3>Chance of precipitation: {WeatherInfo[name][0].days[value].wind}</h3>
+          </div>
+          <h2>Hourly Forecast:</h2>
+          <div className ="todayBot">
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[0].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[0].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[1].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[1].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[2].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[2].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[3].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[3].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[4].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[4].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[5].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[5].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[6].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[6].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[7].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[7].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[8].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[8].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[9].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[9].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[10].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[10].tempF}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[11].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[11].tempF}</p>
+            </div>
+          </div>
+        </div>
+      )
+    } else if(isFahrenheit === false){
+      return(
+        <div className="todaySquare">
+          <div className ="todayTop">
+            <h2>Today's Date: {WeatherInfo[name][0].days[0].date}</h2>
+            <h2>Current Temperature: {WeatherInfo[name][0].currentTempC}</h2>
+            <h3>High: {WeatherInfo[name][0].days[value].highC}</h3>
+            <h3>Low: {WeatherInfo[name][0].days[value].lowC}</h3>
+            <h3>Chance of precipitation: {WeatherInfo[name][0].days[value].chance}</h3>
+            <h3>Chance of precipitation: {WeatherInfo[name][0].days[value].wind}</h3>
+          </div>
+          <h2>Hourly Forecast:</h2>
+          <div className ="todayBot">
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[0].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[0].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[1].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[1].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[2].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[2].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[3].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[3].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[4].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[4].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[5].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[5].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[6].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[6].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[7].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[7].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[8].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[8].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[9].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[9].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[10].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[10].tempC}</p>
+            </div>
+            <div>
+              <p>{WeatherInfo[name][0].days[value].hours[11].hour}:</p>
+              <p>{WeatherInfo[name][0].days[value].hours[11].tempC}</p>
+            </div>
+          </div>
+        </div>
+        
+      )
+    }
+  }
+
+  function renderTodaysFahrenheitTile(i){
+      return <TodaysTile
+      value = {i}
+      />
+  }
+
   function FahrenheitWeatherTile({value}){
     if(name === ""){
       return(
@@ -31,6 +176,7 @@ function Weather() {
     } else {
       return(
         <div className="square">
+          <h2>{WeatherInfo[name][0].days[value].date}</h2>
           <h2>Current Temperature: {WeatherInfo[name][0].currentTempF}</h2>
           <h3>High: {WeatherInfo[name][0].days[value].highF}</h3>
           <h3>Low: {WeatherInfo[name][0].days[value].lowF}</h3>
@@ -51,6 +197,7 @@ function Weather() {
     } else {
       return(
         <div className="square">
+          <h2>{WeatherInfo[name][0].days[value].date}</h2>
           <h2>Current Temperature: {WeatherInfo[name][0].currentTempC}</h2>
           <h3>High: {WeatherInfo[name][0].days[value].highC}</h3>
           <h3>Low: {WeatherInfo[name][0].days[value].lowC}</h3>
@@ -71,7 +218,7 @@ function Weather() {
   
   function CitySelect({onClick}){
     return(
-      <div>
+      <div className="citySearch">
         <input id="citySelect" placeholder={name}>
         
         </input>
@@ -102,8 +249,14 @@ function Weather() {
       <div className="container">
           {renderCitySelect()}
           {renderChangeDegreeTypeBtn()}
+          {renderTodaysFahrenheitTile(0)}
         <div className="currentWeather">
-          {renderWeatherTile(0)}
+          {renderWeatherTile(1)}
+          {renderWeatherTile(2)}
+          {renderWeatherTile(3)}
+          {renderWeatherTile(4)}
+          {renderWeatherTile(5)}
+          {renderWeatherTile(6)}
         </div>
       </div>
     )
